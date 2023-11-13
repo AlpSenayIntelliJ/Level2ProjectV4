85fa604383e21e8c57b302de4af3d5a7a7642067
Alp Senay <alps2789@nycstudents.net>
2023-11-12 20:44:42 -0500

    Working...

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var items = [];
-    var emptyItems = [];
+var items = [];
+var emptyItems = [];
 
-    function addKey() {
-        if (items.indexOf("key") === -1) {
-            items.push("key");
-        }
+function addKey() {
+    if (items.indexOf("key") === -1) {
+        items.push("key");
     }
+}
 
-    function removeAllItems() {
-        items = emptyItems;
-    }
+function removeAllItems() {
+    items = emptyItems;
+}
 
-    // buttons
-    var keyButton = document.getElementById("takeKey");
-    var restartButton = document.getElementById("restartBtn");
-    var unlockButton = document.querySelector(".unlock");
+// buttons
+var keyButton = document.getElementById("takeKey");
+var restartButton = document.getElementById("restartBtn");
+var enterBasementButton = document.getElementById("enterBasement");
+var unlockButton = document.getElementById("unlock");
 
-    var keySentence = document.getElementById("benchHeading");
+var keySentence = document.getElementById("benchHeading");
 
-    function changeTextContent(text) {
-        keySentence.innerHTML = '<h1><span>' + text + '</span></h1>';
-    }
+function changeTextContent(text) {
+    keySentence.innerHTML = '<h1><span>' + text + '</span></h1>';
+}
 
-    keyButton.addEventListener('click', () => {
-        changeTextContent("You picked up a key!");
-        addKey();
-        // for checking items (DELETE LATER)
-        console.log(items);
-    });
+keyButton.addEventListener('click', () => {
+    changeTextContent("You picked up a key!");
+    addKey();
+    // for checking items (DELETE LATER)
+    console.log(items);
+});
 
-    restartButton.addEventListener('click', () => {
-        removeAllItems();
-    });
-    
-    console.log(unlockButton);
-    unlockButton.addEventListener('click', () => {
-        var conditionMet = hasKey();
-        if (!conditionMet) {
-            console.log("You need a key to unlock the door.");
-        } else {
-            removeAllItems();
-            console.log("Door unlocked!");
-            // Add logic to proceed with unlocking, e.g., navigating to another page
-        }
-    });
+restartButton.addEventListener('click', () => {
+    removeAllItems();
+});
 
-    function hasKey() {
-        return items.includes("key");
+enterBasementButton.addEventListener('click', () => {
+    var conditionMet = hasKey();
+    if (!conditionMet) {
+        unlockButton.disabled = true;
     }
 });
+
+unlockButton.addEventListener('click', () => {
+    removeAllItems();
+});
+
+function hasKey() {
+    return items.includes("key");
+}
